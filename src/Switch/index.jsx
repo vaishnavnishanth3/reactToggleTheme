93fb84a366a2590ec1dfffc33b158d716ac81b36
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-05-03 19:04:52 +0530

    Final Commit

--- src/Switch/index.jsx
+++ src/Switch/index.jsx
@@ -2,12 +2,13 @@ import "./styles.css";
 import { useTheme } from "../ThemeContext";
 
 const Switch = () => {
-  const { theme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <label className="switch">
       <input
         type="checkbox"
         checked={theme === "light"}
+        onChange={toggleTheme}
       />
       <span className="slider round" />
     </label>
